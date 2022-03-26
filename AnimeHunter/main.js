e49d1a18b2bd305dda06d3fc195342efda545d98
Jesse Luz <102062848+KilluaTriste@users.noreply.github.com>
2022-03-26 13:23:49 -0300

    Add files via upload

---
+++ AnimeHunter/main.js
@@ -0,0 +1,16 @@
+$('.slider').slick({
+    infinite: true,
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 1000,
+    autoplay: false,
+    autoplaySpeed: 3000
+  });
+
+
+  $('.multi-slide').slick({
+    infinite: false,
+    slidesToShow: 6,
+    slidesToScroll: 1
+  });
